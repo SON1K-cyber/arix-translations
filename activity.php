99d973d7e88cdbda9390c91d7317c5464def02fd
Hassaan <100468795+SON1K-cyber@users.noreply.github.com>
2024-02-05 22:26:14 -1200

    Update activity.php

--- activity.php
+++ activity.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'account-activity-log' => 'Account Activity Log',
-    'activity-log' => 'Activity Log',
-    'clear-filters' => 'Clear Filters',
-    'metadata' => 'Metadata',
-    'close' => 'Close',
-    'no-logs-server' => 'No activity logs available for this server.'
+    'account-activity-log' => 'اکاؤنٹ سرگرمی کا لاگ',
+    'activity-log' => 'سرگرمی کا لاگ',
+    'clear-filters' => 'فلٹر صاف کریں',
+    'metadata' => 'میٹا ڈیٹا',
+    'close' => 'بند کریں',
+    'no-logs-server' => 'اس سرور کے لئے کوئی سرگرمی لاگ دستیاب نہیں ہیں۔'
 ];
